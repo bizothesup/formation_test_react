14810720bc853c7e401330456d5540a712f05d31
SARR Mbaye Babacar <mbayesarr.baba@gmail.com>
2023-10-02 10:14:10 +0200

    show diference beforeeach aftereach beforeAll,AfterAll

--- src/__test__/first.test.js
+++ src/__test__/first.test.js
@@ -1,19 +1,38 @@
 import {additional,soustraction,multiplication} from '../operations/addition'
+let myVariable =1;
+beforeAll(() => {
+  console.log('This before All function')
+})
+afterAll(() => {
+    console.log('This after All function')
+})
+beforeEach(()=>{
+    console.log('je suis au début des test');
+    myVariable=1;
+})
 
+afterEach(()=>{
+    console.log('je suis à la fin des test')
+    console.log('My variable',myVariable)
+    myVariable--;
+})
 describe("Opertaion test ", () => {
     test("Addion", () => {
         expect( additional(1,2)).toBe(3);
         expect( additional(8,13)).toBe(21);
+        console.log("operation addtion");
+        myVariable++;
     });
 
     test("Soustraction", () => {
         expect(soustraction(1,2)).toBe(-1);
         expect(soustraction(586,41)).toBe(545);
+        console.log("operation soustraction");
 
     });
     test("Multplication", () => {
         expect(multiplication(0,2)).toBe(0);
         expect(soustraction(586,41)).toBe(545);
-
+        console.log("operation Multiplication");
     });
 });
